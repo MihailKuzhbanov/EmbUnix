b938d9234d7e71f86ab237b0fec102ea7e2c90e0
Mihail <scrayerful@gmail.com>
2019-10-24 18:18:23 +0500

    Added deploy commands

--- InstallBeremiz.sh
+++ InstallBeremiz.sh
@@ -1,3 +1,4 @@
+#target n host
 sudo apt-get install build-essential bison flex autoconf
 sudo apt-get install python-wxgtk3.0 pyro mercurial
 sudo apt-get install python-nevow python-matplotlib python-lxml python-zeroconf python-cycler
@@ -10,7 +11,12 @@ cd ~/Beremiz/matiec
 autoreconf -i
 ./configure
 make
-sudo apt-get install pip
+sudo apt-get install python-pip
 pip install future
 cd ~/Beremiz/beremiz
+#only host
 python Beremiz.py
+#only target
+python Beremiz_service.py -p 62114 -x 0
+#standalone target
+python Beremiz_service.py -p 62114 -x 0 -a 1
