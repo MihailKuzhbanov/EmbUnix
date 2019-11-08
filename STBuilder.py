554341cc2879905917ab632d78816ba85fcbba0d
Mikhail <scrayerful@gmail.com>
2019-11-08 16:34:32 +0500

    Update STBuilder.py

--- STBuilder.py
+++ STBuilder.py
@@ -1,8 +1,21 @@
 #XML Generator
 #Script return .ST file, ready for translation to .C
 
-import PLCOpenEditor as POE
+import wx
+import os, sys, platform, time, traceback, getopt
+import version
 
-app = POE.PLCOpenEditorApp()
-app.Show()
-app.MainLoop()
+from docutil import *
+from IDEFrame import IDEFrame, AppendMenu
+from IDEFrame import TITLE, EDITORTOOLBAR, FILEMENU, EDITMENU, DISPLAYMENU, PROJECTTREE, POUINSTANCEVARIABLESPANEL, LIBRARYTREE, PAGETITLES
+from IDEFrame import EncodeFileSystemPath, DecodeFileSystemPath
+from editors.Viewer import Viewer
+from PLCControler import PLCControler
+from dialogs import ProjectDialog
+from dialogs.AboutDialog import ShowAboutDialog
+
+import PLCControler as PC
+
+filepath = "/home/username/Beremiz/projects/plc.xml"
+
+PC.GenerateProgram(filepath)
