bc31259df0c2a6c78b2fefc980978dac9fcba8a9
Mikhail <scrayerful@gmail.com>
2019-12-09 09:58:50 +0500

    Update main.cpp

--- FaST/PLC_PRAGMA/source/main.cpp
+++ FaST/PLC_PRAGMA/source/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <vector>
+#include <string.h>
 
 #include "resource1.h"
 
@@ -11,42 +12,102 @@
 
 #include "logic.h"
 
-const int ch(32);
-
 IEC_TIME __CURRENT_TIME;
 
+const int ch(4);
+const int wait(9);
+const int sleepTime(1000000);
 unsigned long long ticktime = 1;
 
-int sleepTime(10);
-
-//port >> output
-template <typename T>
-T * ReadPort(threadbone::TbPort<T> &tport) {
-	T * output;
-	output = new T[ch];
-	tport.read(output);
-	return output;
+//Print array
+void ShowData(int * data, int n, std::string msg) {
+	std::cout << msg;
+	for (int i = 0; i < n; i++) {
+		std::cout << " " << *(data+i);
+	}
+	std::cout << std::endl;
 }
 
-//input >> port
+//Return vector for use in TbPort
 template <typename T>
-void WritePort(threadbone::TbPort<T> &tport, T &input, int64_t wait) {
-	tport.write(input, wait);
+std::vector<T> ConvertDataToVector(T * data, int n) {
+	std::vector<T> result;
+	result.reserve(n);
+	for (int i = 0; i < n; i++) {
+		result.push_back(*(data + i));
+	}
+	return result;
 }
 
+//Return array for use in GetData/SetData functions
+template <typename T>
+T * ConvertVectorToData(std::vector<T> data, int n) {
+	T * result = new T[n];
+	for (int i = 0; i < n; i++) {
+		*(result + i) = data.at(i);
+	}
+	return result;
+}
+
+//Read data from port and send to plc logic
+template <typename T>
+void ReadPort(threadbone::TbPort<T> &prt, int n) {
+	std::vector<T> vec;
+	vec.reserve(n);
+	prt.read(vec);
+	T * arr = ConvertVectorToData(vec, n);
+	SetData(arr, n);
+}
+
+//Read data from plc logic and send to port
+template <typename T>
+void WritePort(threadbone::TbPort<T> &prt, int n, int w) {
+	T * data = GetData(n);
+	std::vector<T> dataVec = ConvertDataToVector(data, ch);
+	prt.write(dataVec, w);
+}
 
 int main(int argc, char* argv[]) {
 
-	threadbone::TbPort<int> writer;
-	threadbone::TbPort<bool> reader;
+	int * data = new int[ch];
+	threadbone::TbPort<int> transfer(ch);
+
 	RESOURCE1_init__();
 	writeLogic(1);
+	
 	if (argc > 1) writeLogic(atoi(argv[1]));
-	printf("PLC initialized\n");
+	std::cout << "PLC initialized\n";
 
 	while (true) {
 
+		data = GetData(ch);
+		ShowData(data, ch, "Original data: ");
+
 		RESOURCE1_run__(ticktime);
+		data = GetData(ch);
+		ShowData(data, ch, "After logic: ");
+
+		usleep(sleepTime);
+
+		//manual debug
+		std::cout << "_____WHILE___LOOP___STARTS___HERE_____" << std::endl;
+		int * data;
+		data = GetData(ch);
+		ShowData(data, ch, "Data recieved: ");
+		for (int i = 0; i < ch; i++) {
+			*(data+i) = i;
+		}
+		ShowData(data, ch, "Data changed: ");
+		SetData(data, ch);
+		for (int i = 0; i < ch; i++) {
+			*(data+i) = 0;
+		}
+		ShowData(data, ch, "Data flushed: ");
+		data = GetData(ch);
+		ShowData(data, ch, "Data recieved: ");
+		RESOURCE1_run__(ticktime);
+		data = GetData(ch);
+		ShowData(data, ch, "Data recieved: ");
 		usleep(sleepTime);
 	}
 
