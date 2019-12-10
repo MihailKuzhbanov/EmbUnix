63b29443002d175a7782d8939c8950594da7b222
Mikhail <scrayerful@gmail.com>
2019-12-10 09:21:01 +0500

    Added tests for read/write port
    Note: variable ch depends from XML project (size of input array Channels).
    Functions SetData() and GetData() must be on pureC for success linker works.

--- FaST/PLC_PRAGMA/source/main.cpp
+++ FaST/PLC_PRAGMA/source/main.cpp
@@ -52,8 +52,7 @@ T * ConvertVectorToData(std::vector<T> data, int n) {
 //Read data from port and send to plc logic
 template <typename T>
 void ReadPort(threadbone::TbPort<T> &prt, int n) {
-	std::vector<T> vec;
-	vec.reserve(n);
+	std::vector<T> vec(n,0);
 	prt.read(vec);
 	T * arr = ConvertVectorToData(vec, n);
 	SetData(arr, n);
@@ -69,27 +68,69 @@ void WritePort(threadbone::TbPort<T> &prt, int n, int w) {
 
 int main(int argc, char* argv[]) {
 
-	int * data = new int[ch];
-	threadbone::TbPort<int> transfer(ch);
+	//uncomment for debug section
+	//int * data = new int[ch];
+	//threadbone::TbPort<int> transfer(ch);
+
+	threadbone::TbPort<int> input(ch);
+	threadbone::TbPort<int> output(ch);
+
+	std::vector<int> inputData(ch, 0);
+	std::vector<int> outputData(ch, 0);
 
 	RESOURCE1_init__();
-	writeLogic(1);
 	
-	if (argc > 1) writeLogic(atoi(argv[1]));
+	writeLogic((argc > 1) ? atoi(argv[1]) : 1);
 	std::cout << "PLC initialized\n";
 
 	while (true) {
 
+		ReadPort(input, ch);
+		RESOURCE1_run__(ticktime);
+		WritePort(output, ch, wait);
+		usleep(sleepTime);
+
+		/////////////////////////
+
+		//Set logic data manually and grab result to vector via TbPort
+		/*
+		std::vector<int> v{1,2,3,4};
+
+		SetData(ConvertVectorToData(v, ch), ch);
+		ShowData(ConvertVectorToData(v, ch), ch, "Vector data: ");
+
+		RESOURCE1_run__(ticktime);
+
+		WritePort(transfer, ch, wait);
+		transfer.read(v);
+		ShowData(ConvertVectorToData(v, ch), ch, "After logic: ");
+
+		usleep(sleepTime);
+		*/
+		/////////////////////////
+
+		//Send vector to logic example
+		/*
+		std::vector<int> v{2,7,1,8};
 		data = GetData(ch);
 		ShowData(data, ch, "Original data: ");
 
+		transfer.write(v,wait);
+		ReadPort(transfer, ch);
+		data = GetData(ch);
+		ShowData(data, ch, "Wrotten data: ");
+
 		RESOURCE1_run__(ticktime);
+
 		data = GetData(ch);
 		ShowData(data, ch, "After logic: ");
 
 		usleep(sleepTime);
+		*/
+		/////////////////////////
 
-		//manual debug
+		//Get and Set logic data example
+		/*
 		std::cout << "_____WHILE___LOOP___STARTS___HERE_____" << std::endl;
 		int * data;
 		data = GetData(ch);
@@ -109,6 +150,8 @@ int main(int argc, char* argv[]) {
 		data = GetData(ch);
 		ShowData(data, ch, "Data recieved: ");
 		usleep(sleepTime);
+		*/
+		/////////////////////////
 	}
 
 	return 0;
